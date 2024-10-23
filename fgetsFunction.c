aed40f335e6287a590fc65de6a4aac4294427111
Ozan Arif Yavuz <yaoz@mhdirekt.local>
2024-10-23 16:22:20 +0200

    Updating the fgetsFunction.c

--- fgetsFunction.c
+++ fgetsFunction.c
@@ -2,20 +2,21 @@
 
 int main(int argc, char const *argv[]) {
     /* declaring the variable of the program */
-    char userInput[100];
+    char userName[25];
     int userAge;
 
     /* requesting the varibles from the user: not with scanf(), but fgets() */
-    printf("Please enter your name and the desired nick name, correctly: ");
-    fgets(userInput, sizeof(userInput), stdin);
+    printf("Please enter your name, correctly: ");
+    // fgets(userInput, sizeof(userInput), stdin);
+    scanf("%[^\n]25s", userName);
 
     /* requesting the user age with fgets() function again */
     printf("How old are you?: ");
-    /* fgets(userAge, sizeof(userAge), stdin); -> It gives error during compiling the program because of the variable type. */
+    // fgets(userAge, sizeof(userAge), stdin); -> It gives error during compiling the program because of the variable type.
     scanf("%d", &userAge);
 
     /* printing the result on the screen */
-    printf("Hello, %s! You're %d years old! Are you ready to play some funny games?", userInput, userAge);
+    printf("Hello, %s! You are %d years old, and congratulations for the successful registration!", userName, userAge);
     
     return 0;
 }
