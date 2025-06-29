ca27e85acd35a24c253e3beb82548dafe593f6eb
Ozan Arif Yavuz <ozanarifyavuz@gmail.com>
2025-06-29 16:34:56 +0200

    The last programs have been deleted and re-written again to catch up the content on Coursera

--- 2025/ReadingFile.c
+++ 2025/ReadingFile.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     char line[1000];
     FILE *hand;
     hand = fopen("romeo.txt", "r");
     while (fgets(line, 1000, hand) != NULL) {
-        printf("%s\n", line);
+        printf("%s", line);
     }
     return 0;
 }
--- 2025/lineInput.C
+++ 2025/lineInput.C
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     char line[1000];
     printf("Enter line: ");
     fgets(line, 1000, stdin);
-    printf("Line: %s", line);
+    printf("Line: %s\n ", line);
     return 0;
 }
