cef1d7906c0295cbaff7405b80e931289120129e
Ozan Arif Yavuz <ozanarifyavuz@gmail.com>
2025-11-23 18:41:49 +0100

    All progresses have been deleted to finish the related course from scratch

--- 2025/lineInput.C
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char const *argv[]) {
-    char line[1000];
-    printf("Enter line: ");
-    fgets(line, 1000, stdin);
-    printf("Line: %s\n ", line);
-    return 0;
-}
